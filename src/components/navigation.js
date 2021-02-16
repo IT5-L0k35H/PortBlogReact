6691f44b74ebaaa4542bf9447ce117b09f76fdfe
Lokesh Verma <lvlokesh2102@gmail.com>
2021-02-16 23:34:21 +0530

    first commit

---
+++ src/components/navigation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+        <nav className='site-navigation'>
+            <span>Harshdeep Singh</span>
+        </nav>
+    )
+}
